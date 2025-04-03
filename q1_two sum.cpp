b198ef5824a19742f242be73bcf70c3a80d8daae
Prit1611 <202401495@daiict.ac.in>
2025-04-03 15:18:55 +0530

    Update q1_two sum.cpp

--- q1_two sum.cpp
+++ q1_two sum.cpp
@@ -1,25 +1,14 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-
-using namespace std;
-
-vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> m;
-    for (int i = 0; i < nums.size(); i++) {
-        int complement = target - nums[i];
-        if (m.find(complement) != m.end()) {
-            return {m[complement], i};
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
         }
-        m[nums[i]] = i;
+        return {}; 
     }
-    return {};
-}
-
-int main() {
-    vector<int> nums = {2, 7, 11, 15};
-    int target = 9;
-    vector<int> result = twoSum(nums, target);
-    cout << "[" << result[0] << ", " << result[1] << "]";
-    return 0;
-}
+};
